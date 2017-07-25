fc73975e3e163293d13fe23142747815d9e81c7f
Alexander Himchak <alexhimchak@alexanders-mbp.attlocal.net>
2017-07-25 12:07:01 -0700

    Working App

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+// Set up MySQL connection.
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  port: 3306,
+  host: "localhost",
+  user: "cheese",
+  password: "poop",
+  database: "burgers_db"
+});
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('cheeseburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('3x3', true);
